1609db93ad2d65ebee3bdd291c12c921ece285ff
cpustejovsky <charles.pustejovsky@gmail.com>
2019-01-02 13:19:47 -0500

    added examples for any() function; some tidying up, also

--- 30secondsOfCode/arrays.js
+++ 30secondsOfCode/arrays.js
@@ -1,8 +1,10 @@
 //all: it looks like syntatic sugar for the every function. But seems sweet to me!
+console.log('this is for the all() function')
 
 const all = (arr, fn = Boolean) => arr.every(fn);
 
 let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr2 = [1, 1, 1, 1, 1];
 
 console.log(all(arr1, x => x < 11)); //true 
 console.log(all(arr1, x => x < 9)); //false 
@@ -13,15 +15,27 @@ console.log(arr1.every(x => x < 11)); //true
 console.log(arr1.every(x => x < 9)); //false
 
 //allEqual: specialized version of all()
+console.log('this is for the all() function')
 
 const allEqual = arr => arr.every(val => val === arr[0]);
-let arr2 = [1,1,1,1,1];
 
-console.log(allEqual(arr1)); //false
 console.log(allEqual(arr2)); //true
+console.log(allEqual(arr1)); //false
 
 //same as...
 
-console.log(arr1.every(x => x === arr1[0])); //false
 console.log(arr2.every(x => x === arr2[0])); //true
+console.log(arr1.every(x => x === arr1[0])); //false
 
+//any() function: syntatic sugar for the array method some
+console.log('this is for the any() function');
+
+const any = (arr, fn = Boolean) => arr.some(fn);
+
+console.log(any(arr1, (x => x < 1))); //true
+console.log(any(arr1, (x => x < 0))); //false
+
+//same as...
+
+console.log(arr1.some(x => x < 1)); //true
+console.log(arr1.some(x => x < 0)); //false
