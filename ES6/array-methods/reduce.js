eced1a29280c91e4a90cf0780a2358c771325613
cpustejovsky <charles.pustejovsky@gmail.com>
2019-03-14 22:45:53 -0400

    learned some practical uses for the reduce helper

---
+++ ES6/array-methods/reduce.js
@@ -0,0 +1,60 @@
+//condensing an array of values into a single value
+
+var numbers = [ 10, 20, 30];
+
+var sum = 0;
+
+var sumOfNumbers = numbers.reduce(((accumulator, currentValue) => accumulator + currentValue), 0);
+
+console.log(sumOfNumbers);
+
+var primaryColors = [
+    { color: 'red'},
+    { color: 'yellow'},
+    { color: 'blue'}
+];
+
+// var colorReducer = (previous, primaryColor) => previous.push(primaryColor.color);
+
+var colorReducer = function(previous, primaryColor){
+    previous.push(primaryColor.color)
+    /*
+    Needs this return function or I get `TypeError: Cannot read property 'push' of undefined`
+    TODO: clean this up to use fat arrow function
+    */
+    return previous;
+};
+
+togetherPrimaryColors = primaryColors.reduce(colorReducer, []);
+
+console.log(togetherPrimaryColors);
+
+//Balanced Parentheses Problem
+/*
+()()()()() === balanced
+((((())))) === balanced
+))))) === unbalanced
+((((( === unbalanced
+)( === unbalanced
+)()( === unbalanced
+*/
+
+var balancedParens = (string) => {
+    //split the string into an array of characters then reduce
+    return !string.split("").reduce((previous, char) => {
+    // have a counter of 0 and add 1 for "(" and subtract 1 ")" and make sure counter remains 0
+    if (previous < 0) {
+        return previous;
+    }
+    if (char === "(") {
+        return ++previous;
+    } if (char === ")") {
+        return --previous;
+    }
+    return previous;
+    }, 0);
+}
+
+console.log(balancedParens("(((("));
+console.log(balancedParens("(())"));
+console.log(balancedParens(")("));
