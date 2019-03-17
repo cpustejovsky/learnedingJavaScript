5a72f1d6f880f28c8a46f60f6089234c644de4d1
cpustejovsky <charles.pustejovsky@gmail.com>
2019-03-17 16:57:08 -0400

    Solving exercise from S.Grider's ES6 course. I didn't figure out the solution before giving up and finding someone else's work, so I went through and commented on each action of the function in an effort to better understand. During some additional testing with objects, I learned that objects are judged equal by reference and not value which is why I was getting surprising results.

--- ES6/array-methods/reduce.js
+++ ES6/array-methods/reduce.js
@@ -54,22 +54,69 @@
 // var totalDistance = trips.reduce(((accumulator, currentValue) => currentValue.distance + accumulator), 0);
 // console.log(totalDistance);
 
-var desks = [
-    { type: 'sitting' },
-    { type: 'standing' },
-    { type: 'sitting' },
-    { type: 'sitting' },
-    { type: 'standing' }
-  ];
-  
-var deskTypes = desks.reduce((accumulator, desk) => {;
-    if (desk.type === 'sitting') {
-        ++accumulator.sitting;
-    }
-    if (desk.type === 'standing') {
-        ++accumulator.standing;
-    }
-    return accumulator;
-}, { sitting: 0, standing: 0 });
+// var desks = [
+//     { type: 'sitting' },
+//     { type: 'standing' },
+//     { type: 'sitting' },
+//     { type: 'sitting' },
+//     { type: 'standing' }
+//   ];
 
-console.log(deskTypes);
+// var deskTypes = desks.reduce(
+//     (accumulator, desk) => {
+//         if (desk.type === "sitting") {
+//             ++accumulator.sitting;
+//         }
+//         if (desk.type === "standing") {
+//             ++accumulator.standing;
+//         }
+//         return accumulator;
+//     },
+//     { sitting: 0, standing: 0 }
+// );
+
+// console.log(deskTypes);
+
+/*
+Another really hard one!  Write a function called 'unique' that will remove all the duplicate values from an array.
+For example, given the following array:
+`var numbers = [1, 1, 2, 3, 4, 4];`
+Your function should return
+`[1,2,3,4]`
+Hint: Use the 'reduce' and 'find' helpers.
+*/
+var testObject = {test: 123};
+var numbers = [8, ,'theotokos', true, 0, 1, 1, testObject.test, 2, 3, 4, 4, 1, 5, 2, 3, 6, 0, 1, 'theotokos', true, testObject.test];
+
+function unique(array) {
+    /* First, make sure the function returns a reduced array*/
+    return array.reduce((accumulator, currentValue) => {
+        /* 
+        local variable determines if element exists by finding an element in 
+        the array and comparing it to any elements that have already been
+        added to the array named `accumulator`
+
+        the find method only returns something if it already exists in the array
+        as a result, the only things that need to be pushed into the accumulator
+        array are things that return undefined.
+
+        Regardless of what is first hit in the array, it'll be added because
+        there isn't anthing to compare it to so it will return an undefined
+
+        From then on, it will only return undefined for things it hasn't seen before.
+        Any other `8`s in the numbers array will be ignored.
+        */
+        let elementExists = accumulator.find(searchingElement => {
+            return searchingElement === currentValue;
+        });
+        console.log(elementExists);
+        if (elementExists === undefined) {
+            accumulator.push(currentValue);
+        }
+
+        return accumulator;
+    }, []);
+}
+console.log(unique(numbers));
+
+console.log({"a": "b"} === {"a": "b"});
