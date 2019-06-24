f5dbb2830fa83290c17625ca28a20b38181d0e5e
cpustejovsky <charles.pustejovsky@gmail.com>
2019-06-24 13:57:14 -0400

    edabit project I was/am working on

--- edabit.js
+++ edabit.js
@@ -1,29 +1,34 @@
 //https://edabit.com/challenge/MDixWSYxH5JZX3xo3
 
+const deepFlatten = arr =>
+    [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
+
 const birdCode = arr => {
+    let acronymArray = [];
     for (let i = 0; i < arr.length; i++) {
         let tempArray = arr[i].split(" ");
         for (let j = 0; j < tempArray.length; j++) {
             if (tempArray[j].includes("-")) {
-                tempArray[j] = tempArray[j].split("-");
+                tempArray[j] = tempArray[j].replace("-", " ");
+                console.log(tempArray);
             }
         }
         if (tempArray.length === 1) {
             resultString = tempArray[0].slice(0, 4).toUpperCase();
-            console.log(resultString);
+            acronymArray.push(resultString);
         }
         if (tempArray.length === 2) {
             resultString =
                 tempArray[0].slice(0, 2).toUpperCase() +
                 tempArray[1].slice(0, 2).toUpperCase();
-            console.log(resultString);
+            acronymArray.push(resultString);
         }
         if (tempArray.length === 3) {
             resultString =
                 tempArray[0].slice(0, 1).toUpperCase() +
                 tempArray[1].slice(0, 1).toUpperCase() +
                 tempArray[2].slice(0, 2).toUpperCase();
-            console.log(resultString);
+            acronymArray.push(resultString);
         }
         if (tempArray.length === 4) {
             resultString =
@@ -31,15 +36,16 @@ const birdCode = arr => {
                 tempArray[1].slice(0, 1).toUpperCase() +
                 tempArray[2].slice(0, 1).toUpperCase() +
                 tempArray[3].slice(0, 1).toUpperCase();
-            console.log(resultString);
+            acronymArray.push(resultString);
         }
     }
+    console.log(acronymArray);
 };
 
-birdCode(["Black-Capped Chickadee", "Common Tern"]); // ["BCCH", "COTE"]
 birdCode(["American Redstart", "Northern Cardinal"]); // ["AMRE","NOCA"]
 birdCode(["Bobolink", "American White Pelican"]); // ["BOBO","AWPE"]
 birdCode(["Great American White Pelican"]); // ["GAWP"]
+birdCode(["Black-Capped Chickadee", "Common Tern"]); // ["BCCH", "COTE"]
 
 //needs to split along "-" also
 // let resultString;
