933c363bd56757264d61896af6e9df5ec4913f22
cpustejovsky <charles.pustejovsky@gmail.com>
2019-07-24 15:24:06 -0400

    implement promises; working on formating to match with what learnyounode is expecting

--- exercises/LearnYouNodeExercises/jugglingAsync.js
+++ exercises/LearnYouNodeExercises/jugglingAsync.js
@@ -1,51 +1,47 @@
+//TODO:
+
 const http = require("http");
 const bl = require("bl");
 const url1 = process.argv[2];
 const url2 = process.argv[3];
 const url3 = process.argv[4];
 
-http
-  .get(url1, res => {
-    res.setEncoding("utf8");
-    res.pipe(
-      bl((err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(data.toString());
-          http
-            .get(url2, res => {
-              res.setEncoding("utf8");
-              res.pipe(
-                bl((err, data) => {
-                  if (err) {
-                    console.error(err);
-                  } else {
-                    console.log(data.toString());
-                    http
-                      .get(url3, res => {
-                        res.setEncoding("utf8");
-                        res.pipe(
-                          bl((err, data) => {
-                            if (err) {
-                              console.error(err);
-                            } else {
-                              console.log(data.toString());
-                            }
-                          })
-                        );
-                        res.on("error", console.error);
-                      })
-                      .on("error", console.error);
-                  }
-                })
-              );
-              res.on("error", console.error);
-            })
-            .on("error", console.error);
-        }
+const httpRequest = url => {
+  return new Promise((resolve, reject) => {
+    http
+      .get(url, res => {
+        res.setEncoding("utf8");
+        res.pipe(
+          bl((err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data.toString("ascii"));
+            }
+          })
+        );
+        res.on("error", console.error);
       })
-    );
-    res.on("error", console.error);
-  })
-  .on("error", console.error);
+      .on("error", console.error);
+  });
+};
+
+// httpRequest(url1)
+//   .then(results => console.log(results))
+//   .catch(error => console.error(`Error: ${error}`));
+// httpRequest(url2)
+//   .then(results => console.log(results))
+//   .catch(error => console.error(`Error: ${error}`));
+// httpRequest(url3)
+//   .then(results => console.log(results))
+//   .catch(error => console.error(`Error: ${error}`));
+
+const promise = Promise.all([
+  httpRequest(url1),
+  httpRequest(url2),
+  httpRequest(url3)
+]);
+
+promise
+  .then(results => console.log(results))
+  .catch(error => console.error(`Error: ${error}`));
