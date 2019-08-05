96f75455388c157e523b10831c1f886b3004b102
cpustejovsky <charles.pustejovsky@gmail.com>
2019-08-05 13:22:08 -0400

    adding validation checking for character creation

--- exercises/fallout-zork/input.js
+++ exercises/fallout-zork/input.js
@@ -1,14 +1,62 @@
-//TODO: Use promises and await/async to deal with callback hell
-//TODO: Read prompts docs https://www.npmjs.com/package/prompts
 const prompts = require("prompts");
+const Character = {
+  name: "Farkyl",
+  age: 20,
+  class: "Cleric"
+};
+
+let acceptableClasses = [
+  "Bard",
+  "Cleric",
+  "Druid",
+  "Fighter",
+  "Ranger",
+  "Wizard"
+];
+
+const newCharacter = Object.create(Character);
+
+const questions = [
+  {
+    type: "text",
+    name: "charName",
+    message: "What is your name? (Any name is acceptable)",
+    validate: charName =>
+      charName.length > 0 ? true : "Need to put something for your name"
+  },
+  {
+    type: "number",
+    name: "age",
+    message: "How old are you? (between 10 and 800)",
+    validate: age =>
+      age >= 10 && age <= 800 ? true : "Age is too high or too low"
+  },
+  {
+    type: "text",
+    name: "charClass",
+    message: `What class do you want to be?
+    Classes are:
+    Bard
+    Cleric
+    Druid
+    Fighter
+    Ranger
+    Wizard
+    `,
+    validate: function(charClass) {
+      for (let i = 0; i < acceptableClasses.length; i++) {
+        if (charClass === acceptableClasses[i]) return true;
+      }
+      return "Must pick a correct class";
+    }
+  }
+];
 
 (async () => {
-  const response = await prompts({
-    type: "number",
-    name: "value",
-    message: "How old are you?",
-    validate: value => (value < 18 ? `Nightclub is 18+ only` : true)
-  });
-
-  console.log(response); // => { value: 24 }
+  const response = await prompts(questions);
+  newCharacter.name = response.charName;
+  newCharacter.age = response.age;
+  newCharacter.class = response.charClass;
+  console.log(newCharacter);
+  // => response => { username, age, about }
 })();
