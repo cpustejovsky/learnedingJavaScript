9bf0131862d2a5dcecae92dd45d073483ae76258
cpustejovsky <charles.pustejovsky@gmail.com>
2019-08-11 06:08:57 -0400

    some small changes

--- exercises/LearnYouNodeExercises/asyncio.js
+++ exercises/LearnYouNodeExercises/asyncio.js
@@ -13,25 +13,20 @@ const fs = require("fs");
 const path = process.argv[2];
 // const path = "./test.txt";
 
-// fs.readFile(path, (err, data) => {
-//   if (err) throw err;
-//   else {
-//     let count = 0;
-//     parsedData = data.toString();
-//     for (let index = 0; index < parsedData.length; index++) {
-//       if (parsedData[index] === "\n") count++;
-//     }
-//     console.log(count);
-//   }
-// });
-let count = 0;
 fs.readFile(path, (err, data) => {
-  return new Promise((resolve, reject) => {
-    if (err) reject(err);
-    else {
-      resolve(data.toString());
-    }
-  });
-}).then(results => {
-  console.log(results);
+  if (err) throw err;
+  else {
+    parsedData = data.toString();
+    console.log(parsedData);
+  }
 });
+// fs.readFile(path, (err, data) => {
+//   return new Promise((resolve, reject) => {
+//     if (err) reject(err);
+//     else {
+//       resolve(data.toString());
+//     }
+//   });
+// }).then(results => {
+//   console.log(results);
+// });
